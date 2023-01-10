3ef7c97ca8ce32776b8650abb9fdce4776bb0cba
youngee2 <67311685+youngee2@users.noreply.github.com>
2023-01-10 23:09:46 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,8 +1,10 @@
 class Solution {
     public int solution(int hp) {
-        int attack5 = hp/5;
-        int attack3 = (hp-5*attack5)/3;
-        int attack1 = (hp-3*attack3-5*attack5)/1;
-        return attack5+attack3+attack1;
+        int answer = hp/5;
+        hp %=5;
+        answer += hp/3;
+        hp %=3;
+        answer += hp/1;
+        return answer;
     }
 }
