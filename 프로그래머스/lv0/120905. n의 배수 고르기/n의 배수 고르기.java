fee78b7ebc9250537f159ead258128c11f034d31
youngee2 <67311685+youngee2@users.noreply.github.com>
2023-01-15 16:20:03 +0900

    Update n의 배수 고르기.java

--- 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -1,16 +1,19 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, int[] numlist) {
-        ArrayList <Integer> list = new ArrayList<>();
-       for(int i=0; i<numlist.length; i++){
+        int count=0;
+        for(int i=0; i<numlist.length; i++){
             if(numlist[i]%n==0){
-                list.add(numlist[i]);
+                count++;
             }
         }
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
+        int j=0;
+        int answer[]=new int[count];
+        for(int i=0; i<numlist.length; i++){
+            if(numlist[i]%n==0){  
+                answer[j]=numlist[i];
+                j++;
+            }
         }
         return answer;
     }
-}
+}  
