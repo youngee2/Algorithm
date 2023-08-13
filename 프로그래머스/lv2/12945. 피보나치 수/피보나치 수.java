7ce0ff0c0a3fda6165963de23a5d48ca3646252c
youngee2 <67311685+youngee2@users.noreply.github.com>
2023-08-13 17:18:36 +0900

    [level 2] Title: 피보나치 수, Time: 1.66 ms, Memory: 76.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int f1 = 1, f0 = 0, f = 0;
+		if (n >= 2) {
+			for (int i = 1; i < n; i++) {
+				f = (f0 + f1)%1234567;
+				f0 = f1;
+				f1 = f;
+			}
+		}
+        return f%1234567;
+    }
+}
