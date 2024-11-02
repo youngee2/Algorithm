6f6e2086e49c1074760bc45d64f0cb08c77ca3dd
youngee2 <67311685+youngee2@users.noreply.github.com>
2024-11-02 19:55:04 +0900

    [Bronze II] Title: 할리갈리, Time: 348 ms, Memory: 39144 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.java
+++ 백준/Bronze/27160. 할리갈리/할리갈리.java
@@ -7,24 +7,25 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        String answer = "NO";
 
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
-        // 펼쳐진 카드의 개수를 변수 n에 저장
-        int n=Integer.parseInt(br.readLine());
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-        //같은 과일별로 개수를 더함.
-        for(int i=0; i<n; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String card=st.nextToken();
-
-            if(map.containsKey(card)){
-                map.put(card, map.get(card)+Integer.parseInt(st.nextToken()));
-            }else{
-                map.put(card, Integer.parseInt(st.nextToken()));
-            }
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            String key = st.nextToken();
+            int value = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + value);
         }
 
-        // 값이 5가 있으면 YES 출력
-        System.out.println(map.containsValue(5)?"YES":"NO");
+
+        for (String s : map.keySet()) {
+            if (map.get(s) == 5) {
+                answer = "YES";
+            }
+        }
+        System.out.println(answer);
     }
 }
