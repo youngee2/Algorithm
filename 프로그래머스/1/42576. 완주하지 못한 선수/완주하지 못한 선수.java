e99b3529eebdee6bc8c1c772c796b73d179b9d23
youngee2 <67311685+youngee2@users.noreply.github.com>
2024-11-07 17:47:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.38 ms, Memory: 97.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,40 +1,31 @@
 import java.util.*;
-//participant에서 completion이 있는지 체크
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //결과를 저장할 변수
-        String answer=""; 
+        String answer="";
         
-        //hashMap 생성
-        HashMap<String,Integer> list = new HashMap<>();
-       
-        //HashMap에 참가자 리스트 추가. 동명이인이 있을 경우 +1 증가
-        for(int i=0; i<participant.length; i++){
-            if(list.containsKey(participant[i])){
-                list.put(participant[i], list.get(participant[i])+1);
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(String str:participant){
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        
+        
+        for(String str:completion){
+            if(map.get(str)==1){
+                map.remove(str);    
             }else{
-                list.put(participant[i],1);
+                map.put(str,map.getOrDefault(str,0)-1);
+            }
+            
+        }
+        
+        for(String key:map.keySet()){
+            if(map.get(key)==1){
+                answer=key;
             }
         }
-        
-        
-        // 참가자 리스트와 완주자 리스트를 비교. 같은 키 값이 있으면 value값 -1 감소.
-        
-        for(String x: completion){
-            list.put(x, list.get(x)-1);
-        }
-        
-       
-        // 참가자 중 0이 아닌 사람을 리턴. 
-        // 전에 같은 이름의 완주자가 있으면 -1 감소했기 때문에 완주자는 0이 나옴. 
-        //완주자는 0이 아닌 수.
-       for(String x:list.keySet()){ 
-           if (list.get(x)!=0){
-                answer=x;
-            }         
-       }
         
         return answer;
     }
 }
-      
