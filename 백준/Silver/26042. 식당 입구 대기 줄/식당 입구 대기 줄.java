861b71a51940d33fdc43bae262f188c0f99868fe
youngee2 <67311685+youngee2@users.noreply.github.com>
2024-11-15 08:41:59 +0900

    [Silver V] Title: 식당 입구 대기 줄, Time: 336 ms, Memory: 41024 KB -BaekjoonHub

---
+++ 백준/Silver/26042. 식당 입구 대기 줄/식당 입구 대기 줄.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    Deque<Integer> list = new LinkedList<>();
+    int max_waiting = 0;
+    int waiting_student_num=Integer.MAX_VALUE;
+
+    public void addData(int value) {
+        list.offer(value);
+
+        if (list.size() > max_waiting) {
+            max_waiting = list.size();
+            waiting_student_num = value;
+        } else if (list.size() == max_waiting) {
+            waiting_student_num = Math.min(waiting_student_num, value);
+        }
+    }
+
+    public void answer() {
+        list.poll();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        Main m = new Main();
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+
+            if (s == 1) {
+                int num = Integer.parseInt(st.nextToken());
+                m.addData(num);
+            } else {
+                m.answer();
+            }
+        }
+
+        bw.write(m.max_waiting + " " + m.waiting_student_num);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
