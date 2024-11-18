f63de9421e1525d7806034c73ef77b3d854eba8a
youngee2 <67311685+youngee2@users.noreply.github.com>
2024-11-18 20:07:43 +0900

    Time: 6 ms (67.96%), Space: 42.2 MB (81.67%) - LeetHub

---
+++ LeetCode/Easy/2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long pickGifts(int[] gifts, int k) {
+        PriorityQueue<Integer> gift=new PriorityQueue<>(Collections.reverseOrder());
+
+        //queue에 선물 수 저장
+        for(int i:gifts){
+            gift.offer(i);
+        }
+
+        //가장 선물이 많이 들어있는 더미를 제곱근
+        for(int i=0; i<k; i++){
+            gift.offer((int)Math.sqrt(gift.poll()));
+        }
+
+        long sum=0;
+
+        //총 남은 선물 개수.
+         while (!gift.isEmpty()) {
+            sum += gift.poll();
+        }
+        
+    return sum;
+    }
+}
